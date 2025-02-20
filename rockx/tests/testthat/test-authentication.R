629e2387c8db7c0a6d188a98c65aa871227ae560
Emil Rossing <emil@rossing.org>
2025-02-20 13:28:25 +0100

    Added rudimentary tests

--- rockx/tests/testthat/test-authentication.R
+++ rockx/tests/testthat/test-authentication.R
@@ -1,7 +1,19 @@
 test_that("set_username_and_password stores credentials", {
-  set_username_and_password("test_user", "test_pass", "http://test.my.url")
+  suppressMessages(set_username_and_password("test_user", "test_pass", "http://test.my.url"))
   expect_equal(Sys.getenv("ROCKX_USERNAME"), "test_user")
   expect_equal(Sys.getenv("ROCKX_PASSWORD"), "test_pass")
-  expect_equal(Sys.getenv("ROCKX_SERVER_URL"), "http://test.my.url")
+  expect_equal(Sys.getenv("ROCKX_SERVER_URL"), "http://test.my.url") 
+})
+
+
+test_that("set_username_and_password updates credentials", {
+  suppressMessages(set_username_and_password("test_user", "test_pass", "http://test.my.url"))
+  expect_equal(Sys.getenv("ROCKX_USERNAME"), "test_user")
+  expect_equal(Sys.getenv("ROCKX_PASSWORD"), "test_pass")
+  expect_equal(Sys.getenv("ROCKX_SERVER_URL"), "http://test.my.url") 
+  suppressMessages(set_username_and_password("test_userB", "test_passB", "http://test.my.urlB"))
+  expect_equal(Sys.getenv("ROCKX_USERNAME"), "test_userB")
+  expect_equal(Sys.getenv("ROCKX_PASSWORD"), "test_passB")
+  expect_equal(Sys.getenv("ROCKX_SERVER_URL"), "http://test.my.urlB") 
   
 })
